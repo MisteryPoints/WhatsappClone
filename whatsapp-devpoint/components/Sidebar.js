f5bbd117d5ff8445fc2d8b459ffd3a909e736a67
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-10-27 21:26:08 +0000

    adding more icons

--- whatsapp-devpoint/components/Sidebar.js
+++ whatsapp-devpoint/components/Sidebar.js
@@ -1,12 +1,23 @@
-import { Avatar } from "@material-ui/core"
+import { Avatar, IconButton } from "@material-ui/core"
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import styled from "styled-components"
 
 function Sidebar() {
   return (
     <Container>
-        <Header>
-            <UseAvatar />
-        </Header> 
+      <Header>
+        <UseAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon/>
+          </IconButton>
+          <IconButton> 
+            <MoreVertIcon/>
+          </IconButton>
+        </IconsContainer>
+      </Header> 
     </Container>
   )
 }
@@ -18,8 +29,14 @@ const Container = styled.div`
 `
 
 const Header = styled.div`
-
+  display: flex;
+  position: sticky;
 `
 
 const UseAvatar = styled(Avatar)`
+  margin: 10px;
+`
+
+const IconsContainer = styled.div`
+
 `
